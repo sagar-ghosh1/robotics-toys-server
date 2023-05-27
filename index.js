a9f284ab9572724a8d5862a276797cb358af1c90
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-28 02:53:19 +0600

    this is first commit

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ez7uet8.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      client.connect();
+
+
+
+
+
+      const toyCollection = client.db("toyDB").collection("robot");
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
